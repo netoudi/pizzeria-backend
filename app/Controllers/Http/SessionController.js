16d21562c84524d5d82d3d768be604d36ff68c31
netoudi <netoudi@rocketmail.com>
2019-06-27 10:55:23 -0300

    Updating endpoint session

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -3,19 +3,21 @@
 const User = use('App/Models/User')
 
 class SessionController {
-  async store ({ request, auth }) {
+  async store ({ request, response, auth }) {
     const { email, password } = request.all()
-
-    const token = await auth.attempt(email, password)
 
     const user = await User.query()
       .where('email', email)
+      .where('type', 'ADMINISTRATOR')
       .fetch()
 
-    return {
-      ...token,
-      user: user.first()
+    if (user.toJSON().length === 0) {
+      return response.status(401).send({ message: 'Email or password invalid.' })
     }
+
+    const token = await auth.attempt(email, password)
+
+    return { user: user.first(), token }
   }
 }
 
